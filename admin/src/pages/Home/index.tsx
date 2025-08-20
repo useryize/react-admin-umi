debf7b9ec1f2f0f8df4943cd3039285bc1f913c8
useryize <80008100@sf.com>
2025-08-20 16:23:46 +0800

    home

--- admin/src/pages/Home/index.tsx
+++ admin/src/pages/Home/index.tsx
@@ -1,14 +1,10 @@
 import Guide from '@/components/Guide';
-import { trim } from '@/utils/format';
 import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
-import styles from './index.less';
 
 const HomePage: React.FC = () => {
   const { name } = useModel('global');
-  return (
-    <div>132</div>
-  );
+  return <>{name}</>
 };
 
 export default HomePage;
